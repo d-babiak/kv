291ef64901244ddb6126246c7effc8bfa8fed19d
Daniel Babiak <d.babiak@gmail.com>
2019-03-08 19:12:07 -0800

    fclose to flush output; log to file

--- kv.c
+++ kv.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #define LINE_SIZE 255
@@ -13,12 +14,23 @@ bool starts_with(char *s, char *prefix) {
   return *prefix == '\0';
 }
 
+
+int wail(char *line) { 
+  FILE* fp = fopen("the_keys.log", "a");
+  int rc = fputs(line, fp);
+  if (rc == EOF)
+    exit(21);
+  fclose(fp);
+  return rc;
+}
+
+
 char * get_input(char *line) {
   printf("kv> ");
   return fgets(line, LINE_SIZE, stdin);
 }
 
-tree_node_t * handle_input(char *line, tree_node_t *root) {
+tree_node_t * handle_input(char *line, tree_node_t *root, bool persist) {
   if (NULL == line) {
       printf("NULL input\n");
       exit(0);
@@ -36,7 +48,10 @@ tree_node_t * handle_input(char *line, tree_node_t *root) {
   if (starts_with(line, "set ")) {
     char *val = malloc(255); 
     sscanf(line, "set %s %s", key, val);
+    // HOW DO WE MAKE ATOMIC? 
     root = set(root, key, val);
+    if (persist)
+      wail(line);
     printf("%s -> %s\n", key, get(root, key));
     return root;
   }
@@ -57,12 +72,22 @@ tree_node_t * handle_input(char *line, tree_node_t *root) {
 //  printf("key: %s, val: %s\n", key, val);
 //}
 
+tree_node_t * boot(tree_node_t *root) {
+  FILE *fp = fopen("the_keys.log", "r");
+  assert(fp != NULL);
+  char *line = malloc(LINE_SIZE);
+  while ((line = fgets(line, LINE_SIZE, fp)) != NULL) {
+    root = handle_input(line, root, false);
+  }
+  fclose(fp);
+  return root;
+}
+
 int main(int argc, char *argv[]) {
   char *line = malloc(LINE_SIZE);
-  tree_node_t *root = NULL; 
-
+  tree_node_t *root = boot(NULL); 
   while (1) {
     line = get_input(line);
-    root = handle_input(line, root);
+    root = handle_input(line, root, true);
   }
 }
